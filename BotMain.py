2f1832b9e63b0dd73488bbe1f7c12b6352249fc0
cawest1221 <cawest1221@gmail.com>
2013-11-16 15:15:42 -0500

    Added IP Lookup/View Active Connections

--- BotMain.py
+++ BotMain.py
@@ -1,21 +1,23 @@
 from Crypto.Cipher import AES
+import subprocess
 import base64
 import socket
+import urllib
 import time
+import sys
 import os
 
 class Bot:
+
+	BLOCK_SIZE = 16
+	PADDING = '\0'
+	IRC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	def __init__(self, server, channel,port):
 			# Connection Information
 			self.server = server
 			self.channel = channel
 			self.port = port
-			self.IRC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-			#Cryptographic Setup
-			self.BLOCK_SIZE = 16
-			self.PADDING = '\0'
 
 
 	def connect(self, nick, userid):
@@ -39,6 +41,14 @@ class Bot:
 		return message + (self.BLOCK_SIZE - len(message) % self.BLOCK_SIZE) * \
 		self.PADDING
 
+	def get_ip_location(self, ip):
+		return urllib.urlopen('http://api.hostip.info/get_html.php?ip='+ip+\
+			'&position=true').read()
+
+	def get_active_connections(self):
+		cmd = "sudo netstat -alpn | grep :80 | awk '{print $5}' |awk -F: '{print $(NF-1)}' |sort | uniq -c | sort -n"
+		ps =  subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+		return ps.communicate()[0]
 
 	def return_values(self):
 		print "Server: %s" % (self.server)
