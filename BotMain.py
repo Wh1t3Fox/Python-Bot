89aba5f32001d5d75a81fcf167dca3c8f3e8937f
cawest1221 <cawest1221@gmail.com>
2013-11-16 11:34:58 -0500

    Added Encryption

--- BotMain.py
+++ BotMain.py
@@ -1,22 +1,44 @@
+from Crypto.Cipher import AES
+import base64
 import socket
 import time
+import os
 
 class Bot:
 
-
 	def __init__(self, server, channel,port):
+			# Connection Information
 			self.server = server
 			self.channel = channel
 			self.port = port
-			self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.IRC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+			#Cryptographic Setup
+			self.BLOCK_SIZE = 16
+			self.PADDING = '\0'
+
 
 	def connect(self, nick, userid):
-		self.irc.connect((self.server, self.port))
-		self.irc.send('NICK %s%i\r\n' % (nick, userid))
-		self.irc.send('USER %i 8 * :%s\r\n' % (userid, nick))
+		self.IRC.connect((self.server, self.port))
+		self.IRC.send('NICK %s%i\r\n' % (nick, userid))
+		self.IRC.send('USER %i 8 * :%s\r\n' % (userid, nick))
 		time.sleep(4)
-		self.irc.send('JOIN %s\r\n' % (self.channel))
-		
+		self.IRC.send('JOIN %s\r\n' % (self.channel))
+
+	def encryptAES(self, message, key):
+		cipher = AES.new(key)
+		return base64.b64encode(cipher.encrypt(self.pad(message)))
+
+
+	def decryptAES(self, ciphertext, key):
+		cipher = AES.new(key)
+		return cipher.decrypt(base64.b64decode(ciphertext)).rstrip(self.PADDING)
+
+
+	def pad(self, message):
+		return message + (self.BLOCK_SIZE - len(message) % self.BLOCK_SIZE) * \
+		self.PADDING
+
 
 	def return_values(self):
 		print "Server: %s" % (self.server)
