65b7c1c5102dff85f9ce1bb188bf065fcbb18659
cawest1221 <cawest1221@gmail.com>
2013-11-16 16:56:41 -0500

    Migrated from IRC to any server/port

--- BotMain.py
+++ BotMain.py
@@ -14,21 +14,17 @@ class Bot:
 
 	BLOCK_SIZE = 16
 	PADDING = '\0'
-	IRC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	def __init__(self, server, channel,port):
+	def __init__(self, server, ,port):
 			# Connection Information
 			self.server = server
-			self.channel = channel
 			self.port = port
 
 
 	def connect(self, nick, userid):
-		self.IRC.connect((self.server, self.port))
-		self.IRC.send('NICK %s%i\r\n' % (nick, userid))
-		self.IRC.send('USER %i 8 * :%s\r\n' % (userid, nick))
-		time.sleep(4)
-		self.IRC.send('JOIN %s\r\n' % (self.channel))
+		self.SERVER.connect((self.server, self.port))
+
 
 	def encryptAES(self, message, key):
 		iv = Random.new().read(self.BLOCK_SIZE)
@@ -45,20 +41,24 @@ class Bot:
 			print "MESSAGE TAMPERED"
 		iv = base64.b64decode(ciphertext)[:16]
 		cipher = AES.new(key, AES.MODE_CBC, iv)
-		return cipher.decrypt(base64.b64decode(ciphertext)[16:]).rstrip(self.PADDING)
+		return cipher.decrypt(base64.b64decode(ciphertext)[16:])\
+		.rstrip(self.PADDING)
 					
 
 	def get_hmac(self, message, key):
 		digest = hmac.new(key, message, hashlib.sha512).digest()
 		return base64.b64encode(digest)[:16]
 
+
 	def pad(self, message):
 		return message + (self.BLOCK_SIZE - len(message) % self.BLOCK_SIZE) * \
 		self.PADDING
 
+
 	def get_ip_location(self, ip):
 		return urllib.urlopen('http://api.hostip.info/get_html.php?ip='+ip+\
 			'&position=true').read()
+
 
 	def get_active_connections(self):
 		cmd = "sudo netstat -alpn | grep :80 | awk '{print $5}' |awk -F: "\
